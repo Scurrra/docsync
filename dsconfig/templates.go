9ad9f6157511cec3858e071482e397d1b1f78608
Scurrra <iscurrra@gmail.com>
2023-07-12 17:53:46 +0300

    Empty template writer

--- dsconfig/templates.go
+++ dsconfig/templates.go
@@ -1,21 +1,36 @@
 package dsconfig
 
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/Scurrra/docsync/markup/md"
+)
 
 // Create an emty template of type `doctype` for the specified `lang` and `plangs`
-func createEmptyTemplate(lang string, plangs []string, doctype DocType) error {
+func CreateEmptyTemplate(lang string, plangs []string, doctype DocType) error {
 	err_dir := os.Mkdir(lang, os.ModePerm)
 	if err_dir != nil {
 		return err_dir
 	}
 
-	// TODO: template files
+	switch doctype {
+	case MD:
+		doc := md.GenerateEmptyDocumentTemplateMarkdown(plangs)
+		doc_file := md.RenderDocument(doc)
+
+		err_file := ioutil.WriteFile(fmt.Sprintf("%s.md", lang), []byte(doc_file), 0)
+		if err_file != nil {
+			return err_file
+		}
+	}
 
 	return nil
 }
 
 // Create template for the specified `lang` and `plangs` using <base>/docs.<doctype> as reference
-func createTemplateFromBase(lang string) error {
+func CreateTemplateFromBase(lang string) error {
 	err_dir := os.Mkdir(lang, os.ModePerm)
 	if err_dir != nil {
 		return err_dir
